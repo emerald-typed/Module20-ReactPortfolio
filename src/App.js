ff76c994cec851168cc83b7b8ccdb9bd10993ebe
kyle <kylehippledev@gmail.com>
2023-03-08 18:31:01 -0600

    Workign on tabs with tutor

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
-import Main from './components/Main.js';
+import NavBar from './components/Nav.js';
 import { Route , Routes} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-      <Main />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <div className='row mx-auto' height-max={70} >
+        <h1 className='m-3'>
+          Kly-Design</h1>
+      </div>
+    </header>
+    
+      <NavBar />
+        <div>
+          <Routes>
+            <Route path='/' element={<h1>hi</h1>} />
+            <Route path='/About' element={<h1>Cool Line fill with react bootstrap later</h1>} />
+            <Route path='/Portfolio' element={<h1>Porty</h1>} />
+            <Route path='/Contact' element={<h1>Cont</h1>} />
+          </Routes>
+        </div>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,46 +1,69 @@
-// import Tab from 'react-bootstrap/Tab';
-// import Tabs from 'react-bootstrap/Tabs';
-// import logo from '../logo.svg';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+// // import logo from '../logo.svg';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 
 function NavBar() {
   return (
     <>
-    <div className='row mx-auto' height-max={70} >
-    <h1 className='m-3'> 
-    {/* <img src={logo} width={50} height={50} className="App-logo" alt="logo" /> */}
-    Kly-Design</h1>
-    </div>
-    <Nav variant="pills" defaultActiveKey="/Home">
-      <PathMod href="/About">About</PathMod>
+      {/* <Nav variant="pills" defaultActiveKey="/Home">
+      <useCustomLink to = "/" >Home</useCustomLink>
+      <customLink to="/About>">About Me</customLink>
       <Nav.Item>
-        <Nav.Link href="/Home">Home</Nav.Link>
+        <Nav.Link to="/Home">Home</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/About">About Me</Nav.Link>
+        <Nav.Link to="/About">About Me</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/Portfolio">Portfolio</Nav.Link>
+        <Nav.Link to="/Portfolio">Portfolio</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/Contact">Contact Me</Nav.Link>
+        <Nav.Link to="/Contact">Contact Me</Nav.Link>
       </Nav.Item>
-    </Nav>
+    </Nav> */}
 
-{/*     
-    <Tabs className="m-2 justify-content-center" variant="pills" defaultActiveKey="/home">
-      <Tab eventKey="link-0" title='Home'>Home</Tab>
-      <Tab eventKey="link-1" title='About Me'>About Me</Tab>
-      <Tab eventKey="link-2" title='Portfolio'>Portfolio</Tab>
-      <Tab eventKey="link-3" title='Contact Me'>Contact Me</Tab>
-    </Tabs> */}
+
+      <Nav className="m-2 justify-content-center" variant="pills" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link eventKey="link-0" title='HomeButton' ><Link to="/">Home</Link></Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1" title='About' ><Link to="/About">About</Link></Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2" title='Port' ><Link to="/Portfolio">Portfolio</Link></Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-3 " title='Contact' ><Link to="/Contact">Contact</Link></Nav.Link>
+        </Nav.Item>
+        {/* <Tab eventKey="link-1" title='About Me'> <Link to="/About">About</Link></Tab> */}
+
+        {/* <Link to ="Portofolio">
+         <Tab eventKey="link-2" title='Portfolio'>Portfolio</Tab>
+        </Link>
+        <Link to="Contact">
+         <Tab eventKey="link-3" title='Contact Me'>Contact Me</Tab>
+        </Link> */}
+      </Nav>
 
     </>
   );
 }
 
-function customLink ()
+// function useCustomLink ({to , children, ...props}){
+//   const resolvedPath = useResolvedPath(to);
+//   const isActive = useMatch({path : resolvedPath.pathname, end:true });
+//   return (
+//     <Nav.Item className={isActive ? "active" : "" }>
+//       <Link to={to} {...props}>
+//       {children}
+//       </Link>
+//     </Nav.Item>
+//   )
+// }
 
 //https://www.youtube.com/watch?v=SLfhMt5OUPI link for help
 export default NavBar;
