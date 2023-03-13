e9c3c631f1b5c57f72dcae52beb577fdd795dafd
kyle <kylehippledev@gmail.com>
2023-03-13 18:01:38 -0500

    All done except have to add in real data but cats load now based on json info

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,6 +2,31 @@ import '../../App.css';
 import ProjectCard from '../pages/Project';
 import Row from 'react-bootstrap/Row';
 
+const projects = [{
+  "src":"https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg",
+  "title":"cat1",
+  "github":"https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg" ,
+  "deployment" : "https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg"
+},
+{
+  "src":"https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg",
+  "title":"cat2",
+  "gitHub":"https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg" ,
+  "deployment" : "https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg"
+},
+{
+  "src":"https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg",
+  "title":"cat3",
+  "gitHub":"https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg" ,
+  "deployment" : "https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg"
+},
+{
+  "src":"https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg",
+  "title":"cat4",
+  "gitHub":"https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg" ,
+  "deployment" : "https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg"
+}
+]
 
 function Grid() {
   return (
@@ -10,9 +35,7 @@ function Grid() {
       {/* //size and spacing of the cards row of two default based on bootstrap react */}
       
     <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 4 }).map((_, idx) => (
-        <ProjectCard/>
-      ))}
+        <ProjectCard projects={projects}/>
     </Row>
     </div>
   );
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,20 +3,22 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-function ProjectCard() {
+function ProjectCard(project) {
     return (
+        project.projects.map(projectItem => (
         <Col>
             <Card>
-            <Card.Img variant="top" src="https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg" />
+            <Card.Img variant="top" src={projectItem.src} />
             <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Title>{projectItem.title}</Card.Title>
                 <Card.Text > 
-                <Button variant="success" className="m-2" href="#">GitHub</Button> 
-                <Button  variant="success" href="#">Deployment</Button> 
+                <Button variant="success" className="m-2" href={projectItem.github}>GitHub</Button> 
+                <Button  variant="success" href={projectItem.deployment}>Deployment</Button> 
                 </Card.Text>
             </Card.Body>
             </Card>
         </Col>
+        ))
     );
 }   
 
